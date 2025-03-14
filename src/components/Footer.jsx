604ebf80835d19d518f60a1b9a0cda3a16120c89
Eduardo Hernández <jhernandezg1403@gmail.com>
2025-03-13 20:58:49 -0600

    Añade informacion de contacto footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ export function Footer() {
   return (
     <footer className="footer">
       <p>&copy; 2025 Fisioterapias Marcelo y Mónica. Todos los derechos reservados.</p>
+      <p>Creado por Eduardo Hernández Contacto creación de páginas web: 5626818106</p>
     </footer>
   );
 }
