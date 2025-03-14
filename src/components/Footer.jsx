d8cf0e54d2685b13b91836e4a64bfbe22bbd344f
Eduardo Hernández <jhernandezg1403@gmail.com>
2025-03-13 20:56:06 -0600

    Corrige fecha derechos autor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../styles/Footer.css';
 export function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2023 Fisioterapias Marcelo y Mónica. Todos los derechos reservados.</p>
+      <p>&copy; 2025 Fisioterapias Marcelo y Mónica. Todos los derechos reservados.</p>
     </footer>
   );
 }
